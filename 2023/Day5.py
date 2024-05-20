ea4d534f5535566d7b3ea672757439b08d8c61c6
Negroni <152891147+Flyzeh@users.noreply.github.com>
2024-05-20 15:06:45 +0200

    Update Day5.py

--- 2023/Day5.py
+++ 2023/Day5.py
@@ -2,7 +2,7 @@ f = open(r'your file')
 lines = f.read().splitlines()
 f.close()
 
-######################### PART 1 (Working with the example but not with the file) #########################
+######################### PART 1 #########################
 
 tab = []
 for line in lines:
@@ -28,16 +28,15 @@ for k in range(len(seeds)):
             while tab[j][0].isdigit() and j < len(tab) - 1 and not trouve:
                 number = tab[j].split()
                 if int(number[1]) <= int(goodseed) < int(number[1]) + int(number[2]):
-                    number[2] = str(int(goodseed) - int(number[1]))
-                    number[1] = str(int(number[1]) + int(number[2]))
-                    number[0] = str(int(number[0]) + int(number[2]))
+                    ajout = 0
+                    ajout = str(int(goodseed) - int(number[1]))
+                    number[1] = str(int(number[1]) + int(ajout))
+                    number[0] = str(int(number[0]) + int(ajout))
                     if number[1] == goodseed:
                         goodseed = number[0]
                         trouve = True
                         break
                 j += 1
-    tabfinal.append(goodseed)
-
+    tabfinal.append(int(goodseed))
 print("Part1 : " + str(min(tabfinal)))
-
 
