eadc98f61342eb7e10fb224c0b368875f8125771
Negroni <152891147+Flyzeh@users.noreply.github.com>
2024-05-20 14:12:58 +0200

    Update Day5.py

--- 2023/Day5.py
+++ 2023/Day5.py
@@ -1,1 +1,41 @@
+f = open(r'your file')
+lines = f.read().splitlines()
+f.close()
+
+######################### PART 1 (Not Working) #########################
+
+tab = []
+for line in lines:
+    if line == "":
+        line = " "
+    tab.append(line)
+print(tab)
+
+seeds = []
+premier = lines[0].split(":")
+seed = premier[1].split()
+for i in range(len(seed)):
+    seeds.append(seed[i])
+print(seeds)
+
+tabfinal = []
+for k in range(len(seeds)):
+    goodseed = seed[k]
+    for j in range(2, len(tab)):
+        if tab[j][0].isalpha():
+            j += 1
+            trouve = False
+            while tab[j][0].isdigit() and j < len(tab) - 1 and not trouve:
+                number = tab[j].split()
+                if int(number[1]) <= int(goodseed) <= int(number[1]) + int(number[2]):
+                    number[2] = str(int(goodseed) - int(number[1]))
+                    number[1] = str(int(number[1]) + int(number[2]))
+                    number[0] = str(int(number[0]) + int(number[2]))
+                    if number[1] == goodseed:
+                        goodseed = number[0]
+                        trouve = True
+                        break
+                j += 1
+    tabfinal.append(goodseed)
+print("Part1 : " + str(min(tabfinal)))
 
