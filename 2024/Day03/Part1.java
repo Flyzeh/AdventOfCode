6f226c0151fedb1ed33c90a5f44c3412059c1af0
Giani Negroni <152891147+Flyzeh@users.noreply.github.com>
2024-12-05 09:13:57 +0100

    Update Part1.java

--- 2024/Day03/Part1.java
+++ 2024/Day03/Part1.java
@@ -1,1 +1,28 @@
+public class Part1 {
+
+    public static void main(String[] args) {
+        String cheminFichier = "Input/2024/input3.txt";
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(cheminFichier))) {
+            String ligne;
+            int result = 0;
+
+            while ((ligne = bufferedReader.readLine()) != null) {
+
+                Pattern pattern = Pattern.compile("mul\\((\\d+),(\\d+)\\)");
+                Matcher matcher = pattern.matcher(ligne);
+                while (matcher.find()) {
+                    int x = Integer.parseInt(matcher.group(1));
+                    int y = Integer.parseInt(matcher.group(2));
+
+                    result += x * y;
+                }
+            }
+            System.out.println(result);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
 
