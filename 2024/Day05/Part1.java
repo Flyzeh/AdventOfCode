6837e78e8ef76a7e4c65a113a3784826b41fadd7
Giani Negroni <152891147+Flyzeh@users.noreply.github.com>
2024-12-05 09:02:19 +0100

    Create Part1.java

---
+++ 2024/Day05/Part1.java
@@ -0,0 +1,73 @@
+public class Part1 {
+
+    public static void main(String[] args) {
+        String cheminFichier = "";
+        Map<Integer, List<Integer>> before = new HashMap<>();
+        Map<Integer, List<Integer>> after = new HashMap<>();
+        int result = 0;
+        boolean maj = false;
+
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(cheminFichier))) {
+            String ligne;
+            while ((ligne = bufferedReader.readLine()) != null) {
+                if (ligne.isEmpty()) {
+                    maj = true;
+                    continue;
+                }
+
+                if (!maj) {
+                    String[] parts = ligne.split("\\|");
+                    int left = Integer.parseInt(parts[0].trim());
+                    int right = Integer.parseInt(parts[1].trim());
+
+                    if (!before.containsKey(left)) {
+                        before.put(left, new ArrayList<>());
+                    }
+                    if (!after.containsKey(left)) {
+                        after.put(left, new ArrayList<>());
+                    }
+                    if (!before.containsKey(right)) {
+                        before.put(right, new ArrayList<>());
+                    }
+                    if (!after.containsKey(left)) {
+                        before.put(right, new ArrayList<>());
+                    }
+
+                    after.get(left).add(right);
+                    before.get(right).add(left);
+                } else {
+                    String[] parts = ligne.split(",");
+                    int[] majP = new int[parts.length];
+
+                    for (int i = 0; i < parts.length; i++) {
+                        majP[i] = Integer.parseInt(parts[i].trim());
+                    }
+
+                    boolean trié = true;
+
+                    for (int i = 0; i < majP.length; i++) {
+                        for (int j = 0; j < i; j++) {
+                            if (after.get(majP[i]).contains(majP[j]))
+                                trié = false;
+                        }
+
+                        for (int k = i + 1; k < majP.length; k++) {
+                            if (before.get(majP[i]).contains(majP[k]))
+                                trié = false;
+                        }
+
+                    }
+
+                    if (trié)
+                        result += majP[majP.length / 2];
+
+                }
+            }
+
+            System.out.println(result);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
